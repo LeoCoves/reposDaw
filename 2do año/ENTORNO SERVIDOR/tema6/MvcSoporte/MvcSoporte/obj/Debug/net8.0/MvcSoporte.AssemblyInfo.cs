fa74c872402b4cbbeab2b9ac62c58c89f8b89bf8
leo <145478507+leocovess@users.noreply.github.com>
2025-01-07 16:45:41 +0100

    Datos actualizados

--- 2do año/ENTORNO SERVIDOR/tema6/MvcSoporte/MvcSoporte/obj/Debug/net8.0/MvcSoporte.AssemblyInfo.cs
+++ 2do año/ENTORNO SERVIDOR/tema6/MvcSoporte/MvcSoporte/obj/Debug/net8.0/MvcSoporte.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea6847a2b53a100d98285d2485a3261dc9e97df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abe7613d7df40ff78cbc8cf965b3ddc5650091a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
