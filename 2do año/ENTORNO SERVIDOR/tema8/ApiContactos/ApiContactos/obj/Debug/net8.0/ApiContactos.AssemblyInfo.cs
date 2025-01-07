44f8c22392f9c79b880329a2199867a76dfa8cad
leo <145478507+leocovess@users.noreply.github.com>
2025-01-07 22:25:33 +0100

    Datos actualizados

--- 2do año/ENTORNO SERVIDOR/tema8/ApiContactos/ApiContactos/obj/Debug/net8.0/ApiContactos.AssemblyInfo.cs
+++ 2do año/ENTORNO SERVIDOR/tema8/ApiContactos/ApiContactos/obj/Debug/net8.0/ApiContactos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiContactos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea6847a2b53a100d98285d2485a3261dc9e97df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa74c872402b4cbbeab2b9ac62c58c89f8b89bf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiContactos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiContactos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
